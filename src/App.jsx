9e6ce243d2f7f90324bb7cac1f4249af8acc0014
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2025-01-30 03:59:59 +0530

    Add initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='m-8 flex justify-center items-center'>
+        <h1 className='font-extrabold text-pink-400'>Hello React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
